7d3fe807e74348136263dc0b38fcc51558177bbe
Marcin Zdun <mzdun@midnightbits.com>
2013-07-20 11:33:14 +0200

    Simple scene

--- studio/studio.cpp
+++ studio/studio.cpp
@@ -30,6 +30,10 @@
 #include <algorithm>
 #include <map>
 
+#include <scene.hpp>
+#include <camera.hpp>
+#include <block.hpp>
+
 struct Command {
 	const char* name;
 	int (*command)(int, char* []);
@@ -47,7 +51,7 @@ template <size_t N>
 Command* get_cmmd(Command (&commands)[N], int argc, char* argv[])
 {
 	if (argc > 1)
-		for (size_t i = 0; i < array_size(commands); ++i)
+		for (size_t i = 0; i < N; ++i)
 		{
 			if (strcmp(argv[1], commands[i].name) == 0)
 				return commands + i;
@@ -59,7 +63,7 @@ Command* get_cmmd(Command (&commands)[N], int argc, char* argv[])
 			fprintf(stderr, "%s: command missing\n", argv[0]);
 
 		fprintf(stderr, "\nKnown commands are:\n");
-		for (size_t i = 0; i < array_size(commands); ++i)
+		for (size_t i = 0; i < N; ++i)
 			fprintf(stderr, "\t%s\n", commands[i].name);
 
 		return nullptr;
@@ -83,8 +87,16 @@ int main(int argc, char* argv[])
 	return command->run(argc - 1, argv + 1);
 }
 
+using namespace studio;
 
 int test(int argc, char* argv [])
 {
+	Scene scene;
+
+	auto cam = scene.add<Camera>(math::Vertex(0, 500, -1000), math::Vertex(0, 500, 0));
+	auto block = scene.add<Block>(100, 100, 100);
+
+	scene.renderTo(cam.get());
+
 	return 0;
 }
