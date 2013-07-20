04f0cd705ed8036fcf0e8b6987c0d07c7f21ff3b
Marcin Zdun <mzdun@midnightbits.com>
2013-07-20 07:26:21 +0200

    Basic interfaces

---
+++ libstudio/includes/container.hpp
@@ -0,0 +1,63 @@
+﻿/*
+* Copyright (C) 2013 Marcin Zdun
+*
+* Permission is hereby granted, free of charge, to any person
+* obtaining a copy of this software and associated documentation
+* files (the "Software"), to deal in the Software without
+* restriction, including without limitation the rights to use, copy,
+* modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+* BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+* ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+#ifndef __LIBSTUDIO_CONTAINER_HPP__
+#define __LIBSTUDIO_CONTAINER_HPP__
+
+#include "renderable.hpp"
+#include <memory>
+#include <vector>
+
+namespace studio
+{
+	class Container : public Renderable
+	{
+	protected:
+		typedef std::vector<std::shared_ptr<Renderable>> Renderables;
+		Renderables m_children;
+	public:
+		template <typename T, typename... Args>
+		std::shared_ptr<T> emplace_back(Args && ... args)
+		{
+			auto tmp = std::make_shared<T>(std::forward(args)...);
+			auto child = std::static_pointer_cast<Renderable>(tmp);
+
+			auto size = m_children.size();
+			m_children.push_back(child);
+			if (size == m_children.size())
+				return nullptr;
+
+			return tmp;
+		}
+
+		void renderTo(const Camera* cam, const math::Matrix& parent) const override
+		{
+			math::Matrix accumulated = parent * localMatrix();
+			for (auto && child : m_children)
+				child->renderTo(cam, accumulated);
+		}
+	};
+}
+
+#endif //__LIBSTUDIO_CONTAINER_HPP__
---
+++ libstudio/includes/renderable.hpp
@@ -0,0 +1,57 @@
+/*
+* Copyright (C) 2013 Marcin Zdun
+*
+* Permission is hereby granted, free of charge, to any person
+* obtaining a copy of this software and associated documentation
+* files (the "Software"), to deal in the Software without
+* restriction, including without limitation the rights to use, copy,
+* modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+* BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+* ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+#ifndef __LIBSTUDIO_RENDERABLE_HPP__
+#define __LIBSTUDIO_RENDERABLE_HPP__
+
+#include "fundamentals.hpp"
+
+namespace studio
+{
+	class Camera;
+
+	class Renderable
+	{
+		math::Matrix m_local;
+	public:
+
+		virtual ~Renderable() {}
+
+		virtual void renderTo(const Camera* cam, const math::Matrix& parent) const = 0;
+		virtual math::Vector normal() const
+		{
+			static math::Vector def(1, 0, 0); // i-hat
+			return def;
+		}
+
+		virtual bool faces(const Renderable* rhs) const
+		{
+			return math::Vector::cosTheta(normal(), rhs->normal()) < 0.0;
+		}
+
+		const math::Matrix& localMatrix() const { return m_local; }
+	};
+}
+
+#endif //__LIBSTUDIO_RENDERABLE_HPP__
