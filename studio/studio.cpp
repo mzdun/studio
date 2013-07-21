2f512b1894980ba68da7e616d9e041efd6114532
Marcin Zdun <mzdun@midnightbits.com>
2013-07-21 17:27:22 +0200

    Example app

--- studio/studio.cpp
+++ studio/studio.cpp
@@ -33,6 +33,19 @@
 #include <scene.hpp>
 #include <camera.hpp>
 #include <block.hpp>
+#include <platform_api.hpp>
+#include <canvas_types.hpp>
+
+#include <future>
+#include <iomanip>
+
+#define STEREO_CANVAS
+
+#if 1
+typedef studio::CyanMagentaCanvas StereoCanvasType;
+#else
+typedef studio::SideBySideCanvas StereoCanvasType;
+#endif
 
 struct Command {
 	const char* name;
@@ -89,14 +102,78 @@ int main(int argc, char* argv[])
 
 using namespace studio;
 
+void setUp(std::shared_ptr<Scene>& scene)
+{
+	scene->add<Block>(2015, 1235, 1);
+	scene->add<Block>(2015, 1, 1060)->translate(0,    0,  -1060);
+	scene->add<Block>(2015, 1, 1060)->translate(0, 1235,  -1060);
+
+	scene->add<Block>(425, 300, 210)->translate(1365, 0,   -210);
+	scene->add<Block>(425, 300, 210)->translate(940,  0,   -210);
+	scene->add<Block>(425, 300, 210)->translate(515,  0,   -210);
+
+	scene->add<Block>(225, 590, 210)->translate(1790, 0,   -210);
+	scene->add<Block>(225, 590, 210)->translate(1790, 590, -210);
+
+	scene->add<Block>(225, 780, 160)->translate(1565, 400, -160);
+	scene->add<Block>(225, 590, 160)->translate(1340, 590, -160);
+
+	scene->add<Block>(225, 590, 160)->translate(410,  590, -160);
+	scene->add<Block>(225, 780, 160)->translate(185,  400, -160);
+
+	scene->add<Block>(577, 352, 48)->translate(700, 545, -96);
+}
+
+std::shared_ptr<StereoCanvasType> g_canvas;
+
 int test(int argc, char* argv [])
 {
-	Scene scene;
+	PlatformAPI init;
 
-	auto cam = scene.add<Camera>(math::Vertex(0, 500, -1000), math::Vertex(0, 500, 0));
-	auto block = scene.add<Block>(100, 100, 100);
+	auto scene = std::make_shared<Scene>();
+	setUp(scene);
 
-	scene.renderTo(cam.get());
+	math::Vertex camPos { 1007.5, 617.5, -1000 };
+
+#ifdef STEREO_CANVAS
+	auto canvas = std::make_shared<StereoCanvasType>(1400, 800);
+	g_canvas = canvas;
+	auto cam = scene->add<StereoCamera>(1000, 50, camPos, camPos + math::Vertex(0, 0, 100));
+	cam->setCanvas(canvas.get());
+#else
+	auto cam = scene->add<Camera>(1000, camPos, camPos + math::Vertex(0, 0, 100));
+	auto canvas = cam->create_canvas<SimpleCanvas>(1400, 800);
+#endif
+
+	scene->renderAllCameras();
+	canvas->save("test.png");
+
+#if 0
+	std::vector<std::future<void>> tasks;
+
+	for (size_t id = 0; id < 25; ++id)
+	{
+		tasks.push_back(std::move(std::async([=](const math::Vertex& camPos){
+			std::cout << "<" << std::flush;
+			auto cam = scene->add<Camera>(1000, camPos + math::Vertex(50 * id, 0, 0), camPos + math::Vertex(50 * id, 0, 100));
+			auto canvas = cam->create_canvas<BmpCanvas>(1400, 800);
+			scene->renderTo(cam.get());
+
+			wchar_t fname[100];
+			swprintf_s(fname, L"test_%04d.png", id);
+			canvas->save(fname, L"image/png");
+			std::cout << ">" << std::flush;
+		}, math::Vertex{ 507.5, 800, -1000 })));
+	}
+
+	for (auto && f : tasks)
+		f.get();
+
+	tasks.clear();
+#endif
+
+	g_canvas.reset();
+	scene.reset();
 
 	return 0;
 }
